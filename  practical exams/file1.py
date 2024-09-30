25599f8d8fede474303b29ff0b8be91edfeb0805
JibinAji21 <jibinaji26@gmail.com>
2024-09-30 13:53:25 +0530

    practical exam1

---
+++  practical exams/file1.py
@@ -0,0 +1,61 @@
+# 12. how to remove duplicates in a list using for loop ? 
+
+# l=[1,2,3,3,5,5]
+# n=[]
+# for i in l:
+#     if i not in n:
+#         print()
+#         n.append(i)
+# print(n)
+
+
+# 13. Create a calculator using function and modules ?
+
+# def numbers():
+#     a=int(input("enter first number"))
+#     b=int(input("enter second number"))
+#     return a,b
+# def add():
+#     a,b=numbers()
+#     print(a+b)
+# def sub():
+#     a,b=numbers()
+#     print(a-b)
+# def div():
+#     a,b=numbers()
+#     print(a/b)
+# def mul():
+#     a,b=numbers()
+#     print(a*b)
+
+# while True:
+#     print('''
+#           1.Addition
+#           2.Substraction
+#           3.Division
+#           4.Multiplication
+#           5.Exit''')
+    
+#     choice=int(input("enter your choice"))
+#     if choice==1:
+#         add()
+#     elif choice==2:
+#         sub()
+#     elif choice==3:
+#         div()
+#     elif choice==4:
+#         mul()
+#     elif choice==5:
+#         break
+
+
+# 14. Write a python program to print the following pattern ?
+
+# for i in range (4):
+#     for j in range(4):
+#         print("A  B  C")
+
+
+# 15. Write a python function to calculate the factorial of a given number?
+
+# 16. Write a python function that takes a dict and returns a new dict with keys and  values swapped?
